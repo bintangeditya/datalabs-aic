a1674ed6d8f1e8299b74d3f57eeaf0237bc793ea
mrakhaf <muhammadrakhaf@gmail.com>
2021-08-23 12:52:41 +0700

    create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask.globals import request
+import tensorflow as tf
+import json
+from datalabs_chatbot import get_chat_response
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+
+@app.route('/', methods=['POST'])
+def predict():
+    data = request.form['chat']
+    res = get_chat_response(data)
+    response = {
+        "res": res,
+        "status": "success"
+    }
+    response_json = json.dumps(response)
+    print(response_json)
+    return get_chat_response(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++
@@ -1,5 +0,0 @@
-import datalabs_chatbot as chatbot
-
-while True:
-    message = input("")
-    print(chatbot.get_chat_response(message))
